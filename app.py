03fd858a7c8dd5840fe662162bd9dbb2386279ce
Arsalan Ahmad <arsalannahmad01@gmail.com>
2024-03-19 03:57:56 +0530

    fetching jobs urls

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ import random
 import json
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from undetected_chromedriver import Chrome
+import  undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 
 
 def fetch_urls():
@@ -16,10 +15,10 @@ def fetch_urls():
     try:
 
         options = webdriver.ChromeOptions()
-       
-        options.add_argument('--headless')
+        options.add_argument('--headless') 
+        options.add_argument('--disable-gpu')
 
-        driver = Chrome(headless=True)
+        driver = uc.Chrome(options=options)
 
         driver.get(url)
 
@@ -43,20 +42,22 @@ def fetch_urls():
             all_urls.append("https://wellfound.com" + link)
             count += 1
 
-
-       
-
     except Exception as e:
         print('Error:', e)
         driver.save_screenshot("error_screenshot.png")
+        time.sleep(2)
         driver.quit()
+        time.sleep(2)
         fetch_urls()
     finally:
-        time.sleep(5)
+        time.sleep(2)
         driver.save_screenshot("final_screenshot.png")
         driver.quit()
 
     return all_urls
+
+
+
 
 def fetch_company_data(url):
 
@@ -65,13 +66,10 @@ def fetch_company_data(url):
     try:
 
         options = webdriver.ChromeOptions()
-     
         options.add_argument('--headless') 
+        options.add_argument('--disable-gpu')
 
-        # driver = webdriver.Chrome(options=options)
-        driver = Chrome(headless=True)
-
-
+        driver = uc.Chrome(options=options)
         driver.get(url)
 
         time.sleep(5)
@@ -94,7 +92,13 @@ def fetch_company_data(url):
         time.sleep(5)
         company_size_funding = company_data.find('div', {'class':"styles_component__Wb41n styles_component__qhaPy styles_about__6dvji styles_white__yJuQK"}).find('dl').find_all('dt')
         time.sleep(3)
-        company_locations = company_size_funding[1].find_all('a', {'class':'styles_component__UCLp3 styles_defaultLink__eZMqw !text-dark-aaa underline'}).getText()
+        locations = company_size_funding[1].find_all('a', {'class':'styles_component__UCLp3 styles_defaultLink__eZMqw !text-dark-aaa underline'})
+        company_locations = []
+
+        for location in locations:
+            company_locations.append(location.getText())
+
+
         time.sleep(3)
         company_size = company_size_funding[2].getText()
         time.sleep(3)
@@ -106,35 +110,57 @@ def fetch_company_data(url):
         for market in markets:
             company_markets.append(market.find('span').getText())
 
+        types = company_data.find_all('span', {'class':'mb-1 styles-module_component__2E93_ inline-flex flex-row items-center mr-2 last:mr-0 rounded-full align-middle bg-gray-200 text-gray-700 gap-2 text-xs px-3 py-1'})
+        company_types = []
+        for type in types:
+            company_types.append(type.getText())
+
+        jobs_count = soup.find('div', {'class':'flex justify-center gap-1'}).find('span').getText()
+
+        jobs_links = soup.find_all('a', {'class':'styles_component__UCLp3 styles_defaultLink__eZMqw styles_anchor__aTiEC'})
+        company_jobs = []
+        for job_link in jobs_links:
+            company_jobs.append("https://wellfound.com"  + job_link.get('href'))
+
 
         print("Company Name: " + company_name)
         print("About Company: " + company_about)
         print("Company Size: " + company_size)
         print("Company Funding: " + company_funding)
+        print("Company locations: ", company_locations)
+        print("Company markets: ", company_markets)
+        print("Company Types: ", company_types)
+        print('Number of jobs: ' + jobs_count)
+        print('Jobs: ', company_jobs)
 
-        company = {
-            "name":company_name,
-            "About":company_about,
-            "size":company_size,
-            "funding":company_funding,
-            "markets": company_markets,
-            "locations":company_locations
-        }
+        # company = {
+        #     "Name":company_name,
+        #     "About":company_about,
+        #     "Company_size":company_size,
+        #     "Funding":company_funding,
+        #     "Markets": company_markets,
+        #     "Locations":company_locations,
+        #     "Company_type" : company_types,
+        #     "Jobs_count" : jobs_count,
+        #     "Jobs":company_jobs
+        # }
 
-        companyJsonData = json.dumps(company, indent=4)
-    
-       
+        # companyJsonData = json.dumps(company, indent=4)
 
     except Exception as e:
         print('Error:', e)
         driver.save_screenshot("error_screenshot.png")
+        time.sleep(2)
         driver.quit()
+        time.sleep(2)
         fetch_company_data(url)
     finally:
         driver.save_screenshot("final_screenshot.png")
         driver.quit()
 
-    return companyJsonData
+    # return companyJsonData
+
+
 
 
 def main():
@@ -142,6 +168,6 @@ def main():
     print(all_urls)
     for url in all_urls:
         time.sleep(random.uniform(60,  100))
-        print(fetch_company_data(url))
+        fetch_company_data(url)
 
 main()
